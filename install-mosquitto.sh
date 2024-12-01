6c1489e63c95f223511328b0571231c6fb2960a7
Jim Jinn <JimBigJinn@gmail.com>
2024-12-01 18:28:19 +1100

    0.2 pre final

--- install-mosquitto.sh
+++ install-mosquitto.sh
@@ -3,18 +3,10 @@
 # wget -O - https://raw.githubusercontent.com/JimJinn/Linux-public/refs/heads/main/install-mosquitto.sh | bash
 
 # Check if mosquitto is installed
-echo "Version 0.19"
+echo "Version 0.2"
 if ! dpkg -l | grep -q mosquitto; then
     echo "Mosquitto is not installed. Installing..."
     sudo apt install -y mosquitto mosquitto-clients
-
-
-
-    # Publish a test message
-    mosquitto_pub -h $mqtt_server -t "$notify_topic" -m "Mosquitto is installed on $(hostname)"
-else
-    echo "Mosquitto is already installed."
-fi
 
     config="/etc/notificator/notificator.conf"
     mqtt_server="192.168.9.20"
@@ -31,3 +23,10 @@ fi
     echo "NOTIFY_TOPIC=$notify_topic" | sudo tee -a $config > /dev/null
     echo "MAX_MESSAGES=$max_messages" | sudo tee -a $config > /dev/null
     echo "Default MQTT server IP address and topic are written to $config, modify as required"
+
+    # Publish a test message
+    mosquitto_pub -h $mqtt_server -t "$notify_topic" -m "Mosquitto is installed on $(hostname)"
+else
+    echo "Mosquitto is already installed."
+fi
+
