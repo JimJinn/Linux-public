04ba7dac535d25a563c1cc4a58d22570ae63c515
Jim Jinn <JimBigJinn@gmail.com>
2024-12-01 15:53:51 +1100

    server address

--- install-mosquitto.sh
+++ install-mosquitto.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # install-mosquitto
+# wget -O - https://raw.githubusercontent.com/JimJinn/Linux-public/refs/heads/main/install-mosquitto.sh | bash
 
 # Check if mosquitto is installed
 if ! dpkg -l | grep -q mosquitto; then
@@ -8,6 +9,13 @@ if ! dpkg -l | grep -q mosquitto; then
 else
     echo "Mosquitto is already installed."
 fi
+
+# Request user to input Mosquitto server address
+read -p "Enter Mosquitto server address (default: 192.168.9.20): " mqtt_server
+mqtt_server=${mqtt_server:-192.168.9.20}
+
+# Save the server address to a file for future use
+echo "MQTT_SERVER_IP=$mqtt_server" > ~/.mosquitto_server
 
 # Publish a test message
 #mosquitto_pub -h <MQTT_SERVER_IP> -t "homeassistant/sensor/disk_health" -m "Disk: Healthy"
