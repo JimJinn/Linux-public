308232432e97242638bcb8903686cb32e6c9b702
Jim Jinn <JimBigJinn@gmail.com>
2024-12-01 16:11:49 +1100

    variables

--- install-mosquitto.sh
+++ install-mosquitto.sh
@@ -12,8 +12,9 @@ fi
 
 
 # Save the server address to a file for future use
-echo "MQTT_SERVER_IP=192.168.9.20" > ~/.mosquitto_server
-echo "Default MQTT server IP address is written to ~/.mosquitto_server, modify as required"
+echo "MQTT_SERVER_IP=192.168.9.20" > ~/.notificator_config
+echo "NOTIFY_TOPIC=telegram/alex" > ~/.notificator_config
+echo "Default MQTT server IP address and topic are written to ~/.notificator_config, modify as required"
 
 # Publish a test message
 #mosquitto_pub -h <MQTT_SERVER_IP> -t "homeassistant/sensor/disk_health" -m "Disk: Healthy"
