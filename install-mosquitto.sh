2beb18f5fca8b00bb0de30ed94b32e5501b17847
Jim Jinn <JimBigJinn@gmail.com>
2024-12-01 16:14:51 +1100

    Mosquitto is installed

--- install-mosquitto.sh
+++ install-mosquitto.sh
@@ -10,11 +10,14 @@ else
     echo "Mosquitto is already installed."
 fi
 
+mqtt_server="192.168.9.20"
+notify_topic="telegram/alex"
 
 # Save the server address to a file for future use
-echo "MQTT_SERVER_IP=192.168.9.20" > ~/.notificator_config
+# echo "MQTT_SERVER_IP=192.168.9.20" > ~/.notificator_config
+echo "MQTT_SERVER_IP=$mqtt_server" > ~/.notificator_config
 echo "NOTIFY_TOPIC=telegram/alex" >> ~/.notificator_config
 echo "Default MQTT server IP address and topic are written to ~/.notificator_config, modify as required"
 
 # Publish a test message
-#mosquitto_pub -h <MQTT_SERVER_IP> -t "homeassistant/sensor/disk_health" -m "Disk: Healthy"
+mosquitto_pub -h $mqtt_server -t "$notify_topic" -m "Mosquitto is installed on $(hostname)"
