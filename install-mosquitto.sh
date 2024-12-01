8085d0b0ea377246fc9df6f52c6e56c8963b2db0
Jim Jinn <JimBigJinn@gmail.com>
2024-12-01 17:00:22 +1100

    1.13

--- install-mosquitto.sh
+++ install-mosquitto.sh
@@ -3,15 +3,15 @@
 # wget -O - https://raw.githubusercontent.com/JimJinn/Linux-public/refs/heads/main/install-mosquitto.sh | bash
 
 # Check if mosquitto is installed
-echo "Version 1.11"
+echo "Version 1.13"
 if ! dpkg -l | grep -q mosquitto; then
     echo "Mosquitto is not installed. Installing..."
     sudo apt install -y mosquitto mosquitto-clients
 
-    config="/etc/notificator/notificator.conf"
-    mqtt_server="192.168.9.20"
-    notify_topic="telegram/alex"
 
+    echo "MQTT_SERVER_IP=$mqtt_server" > $config
+    echo "NOTIFY_TOPIC=$notify_topic" >> $config
+    echo "Default MQTT server IP address and topic are written to $config, modify as required"
 
     # Publish a test message
     mosquitto_pub -h $mqtt_server -t "$notify_topic" -m "Mosquitto is installed on $(hostname)"
@@ -19,11 +19,12 @@ else
     echo "Mosquitto is already installed."
 fi
 
+    config="/etc/notificator/notificator.conf"
+    mqtt_server="192.168.9.20"
+    notify_topic="telegram/alex"
+
     if [ ! -d "$(dirname "$config")" ]; then
         echo "Creating config directory..."
+        # sudo mkdir -p "/etc/notificator/notificator.conf"
         sudo mkdir -p "$(dirname "$config")"
     fi
-
-    echo "MQTT_SERVER_IP=$mqtt_server" > $config
-    echo "NOTIFY_TOPIC=$notify_topic" >> $config
-    echo "Default MQTT server IP address and topic are written to ~/.notificator_config, modify as required"
